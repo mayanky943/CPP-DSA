cb14eb2c208f4554cdf899e10933319b917a50bb
Mayank <ymayank521@gmail.com>
2022-05-01 01:22:09 +0530

    correct code for dec to bin

--- Basic/decTobin.cpp
+++ Basic/decTobin.cpp
@@ -1,22 +1,25 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
 
-long pow(long base,long power){
-    long ans=1;
-    while(power-->0){
-        ans*=base;
-    }
-    return ans;
-}
-
-
-int main() {
-	// Write your code here
-	long n;
-    cin>>n;
-    string str="";
+string decToBinary(int n)
+{
+	string s;
     while(n>0){
-        
-    }  
+        int temp=n%2;
+        s+=to_string(temp);
+        n/=2;
+    }
+    return s;
+}
+ 
+// driver code
+int main()
+{
+    int n;
+    cin>>n;
+    
+    string res=decToBinary(n);
+    reverse(res.begin(),res.end());
+    cout<<res;
 }
