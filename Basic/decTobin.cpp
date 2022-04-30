9413914f944707a623b4cbcac371a7faa85315a1
Mayank <ymayank521@gmail.com>
2022-04-30 22:34:35 +0530

    Added decToBin

---
+++ Basic/decTobin.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+
+long pow(long base,long power){
+    long ans=1;
+    while(power-->0){
+        ans*=base;
+    }
+    return ans;
+}
+
+int main() {
+	// Write your code here
+	long n;
+    cin>>n;
+    string str="";
+    while(n>0){
+        
+    }
+    
+}
